b59f4672ca8f5fafd0cb33bd72537acf18644e35
KimJesung <andrewkim99@naver.com>
2025-06-17 18:31:31 +0900

    docs: Server.js, app.js port number 수정

--- Server.js
+++ Server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.json({ message: "API 서버가 실행 중입니다!" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`서버가 포트 ${PORT}에서 실행 중입니다`);
 });
--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Study App API Server" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
