680baff69a795c71058105eb3bd28ac73d05c521
KimJesung <andrewkim99@naver.com>
2025-06-17 20:40:51 +0900

    docs: Server.js MongoDB 연결 성공 멘트 수정

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB 연결 성공 1"))
+  .then(() => console.log("MongoDB 연결 성공"))
   .catch((err) => console.error("MongoDB 연결 실패:", err));
 
 // Routes 연결
