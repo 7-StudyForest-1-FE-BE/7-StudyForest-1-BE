37e242d18fbe721c2114bdf9778fb6ed5ddb007e
KimJesung <andrewkim99@naver.com>
2025-06-19 11:12:23 +0900

    docs: bgThemes.js 텍스트 색상 추가 및 이미지 테마 추가

--- src/config/bgThemes.js
+++ src/config/bgThemes.js
@@ -1,23 +1,39 @@
 const bgThemes = [
   {
     type: "color",
-    value: "#ffffff",
-  },
-  {
-    type: "color",
     value: "#E1EDDE",
+    textColor: "#578246",
   },
   {
     type: "color",
     value: "#FFF1CC",
+    textColor: "#C18E1B",
   },
   {
     type: "color",
     value: "#E0F1F5",
+    textColor: "#418099",
   },
   {
     type: "color",
     value: "#FDE0E9",
+    textColor: "#FDE0E9",
+  },
+  {
+    type: "image",
+    value: "bg1.jpg",
+  },
+  {
+    type: "image",
+    value: "bg2.jpg",
+  },
+  {
+    type: "image",
+    value: "bg3.jpg",
+  },
+  {
+    type: "image",
+    value: "bg4.jpg",
   },
 ];
 
